4c3ec3fc274d33592bdef1b470da4af866f71659
Ali-Master <ali_4286@live.com>
2016-11-26 11:58:14 +0330

    start

---
+++ falaki-time.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Calculate hours constellation
+ * @param  {[string]} start [Sunrise time]
+ * @param  {[string]} end   [sunset time]
+ * @return {[string]}
+ */
+var falakiTime = function(start, end) {
+    this.start = start;
+    this.end   = end;
+}
